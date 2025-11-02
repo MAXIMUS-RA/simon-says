be0d22069639e1d3bf188bf4572d308dc7e675f2
Максим Рацілевич <vt231_rmya@student.ztu.edu.ua>
2025-11-02 19:15:58 +0200

    feat: implement routing with react-router and remove RouterContext (#3)

--- src/components/CustomBtn.tsx
+++ src/components/CustomBtn.tsx
@@ -1,17 +1,15 @@
-import { useRouter } from "../context/RouterContext";
+import { NavLink } from "react-router";
 import type { Route } from "../types/routerContext.types";
 
 function CustomBtn({ link, name, ...props }: { link: Route; name: string; [key: string]: any }) {
-    const { navigate } = useRouter();
 
     return (
-        <li
-            {...props}
+        <NavLink
+            to={link}
             className="list-none text-white px-4 py-2 rounded-full border-2 border-white hover:bg-white hover:text-indigo-600 transition duration-200"
-            onClick={() => navigate(link)}
         >
             {name}
-        </li>
+        </NavLink>
     );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ import { useColors } from "../hooks/useColors";
 
 function Header() {
     const links: ILinks[] = [
-        { name: "Home", link: "home" },
-        { name: "Game", link: "game" },
-        { name: "About", link: "about" },
-        { name: "Settings", link: "settings" },
+        { name: "Home", link: "/home" },
+        { name: "Game", link: "/game" },
+        { name: "About", link: "/about" },
+        { name: "Settings", link: "/settings" },
     ];
 
     const [accentColor, setAccentColor] = useState<string>();
@@ -22,14 +22,7 @@ function Header() {
                 <h1 className="text-white text-2xl font-bold tracking-wider">Simon Says</h1>
                 <ul className="flex space-x-6">
                     {links.map((el) => (
-                        // <li key={el.link}>
-                        <CustomBtn
-                            link={el.link}
-                            name={el.name}
-                            key={Math.random() * 10}
-                            // className="text-white px-4 py-2 rounded-full border-2 border-white hover:bg-white hover:text-indigo-600 transition duration-200"
-                        />
-                        // </li>
+                        <CustomBtn link={el.link} name={el.name} key={Math.random() * 10} />
                     ))}
                 </ul>
             </nav>
